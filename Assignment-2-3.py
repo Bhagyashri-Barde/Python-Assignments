0c56b1dcb3497b15e9abd6143250b327ea75000c
bardebhagyashri <bhagyashri@wattmonk.com>
2026-06-12 21:23:04 +0530

    Assignment-2

---
+++ Assignment-2-3.py
@@ -0,0 +1,7 @@
+# Program to take user's name and age and display 
+# output = Hello <name>,you will turn <age+1> next year.
+
+name = input("Enter Name:")
+age = int(input("Enter Age :"))
+print("\n Output : \n")
+print("Hello",name,",you will turn", age+1,"next year")
