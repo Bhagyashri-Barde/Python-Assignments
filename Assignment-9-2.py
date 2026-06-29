fc96a61b06f73f4bf124e580aca906d9356bde42
bardebhagyashri <bhagyashri@wattmonk.com>
2026-06-29 16:13:51 +0530

    Merge branch 'Assignment-9'

---
+++ Assignment-9-2.py
@@ -0,0 +1,16 @@
+# Program - Create One Function ChkGreater()
+# that accepts 2 numbers and print the grater number
+
+def ChekGreater(No1,No2):
+
+    if No1 > No2:
+        print(No1,"is greater")
+    else:
+         print(No2,"is greater")
+
+def main():
+
+    ChekGreater(10,20)
+
+if __name__ == "__main__":
+    main()
